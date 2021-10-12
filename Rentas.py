454952d383788a7371bde87e116ec3a642aea84c
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2021-10-12 15:20:14 -0300

    Update Rentas.py

--- Rentas.py
+++ Rentas.py
@@ -2,11 +2,8 @@
 import csv, os, time
 os.system('cls')
 
-
-# archivo a parsear
 archivo_clientes  = "clientes.txt"
-
-#Campos de la planilla
+archivo_impuestos = "IMPUESTOS.csv"
 FECHA_EMI = 2
 tipo_doc = 80
 DOCUMENTO_INDEX = 5
@@ -24,33 +21,39 @@ tipDEBA = 0
 tipDEBB = 0             
 
 def main():
-#funcion main() lee archivo_impuesto (csv) y llama a la
-#funcion read_file() 
+     #funcion main pasa el archivo de trabajo a la funcion 
+     # read_file    
     file = read_file(archivo_impuestos)
     print(file)
 
 def rellenar_ceros(numero):
-    #rellena el campo la cantidad de ceros requerida
     j="0"
     #length of the number
     length=len(numero)
     #aumento en 1 a length para cuadrar la cantidad de ceros a agregar
     ceros_faltantes = 15 - (length+1)
-    
+    #print(ceros_faltantes," ceros a agregar")
     while ceros_faltantes > 0:
         j="0"+j
         ceros_faltantes = ceros_faltantes-1
-           
+        #print("soy j + numero ",j+numero)
+        #print("largo de j + numero es : ", len(j+numero))
+        #time.sleep(6)
+         
     return j+numero   #'''Ojo con el return, tiene q estar fuera del bucle'''
 
 def my_float(num_fl):
+    #antes
+    print("antes ",type(num_fl))
+    #funcion float conserva tal cual el nro
     flo_ = float(num_fl)
     print(type(flo_))
     print("my",flo_)
     return flo_
 
 def read_file(file_name) :
-    #funcion que abre y carga los datos de un registro a la vez
+    #funcion read_file empieza a cargar cada registro y a 
+    #parsearlo
     imp_gr=""
     with open(file_name, "rt") as filename_csv:
         os.system('cls')
@@ -91,13 +94,16 @@ def read_file(file_name) :
             if tipo_compr =='FAC' and digit=='A':
                 tipFACA = "01"
                 with open("DATOS.TXT", "at") as d:
+                    #comienzo a guardar los datos parseados en el archivo
+                    #datos.txt
                     dt = f"{parsed_date}{tipo_doc}{documen}{tipFACA}{digit}{parse_cod_lug_emi}{num_comp_parsed}{imp}{porcent_alicuo}{imp_gr}"
-                    #print(len(dt))
+                    
                     t = dt.strip()
                     print(t,file=d)
+               
             elif tipo_compr =='FAC' and digit=='B':
                 tipFACB = "06"
-                
+                               
                 with open("DATOS.TXT", "at") as d:
                     dt = f"{parsed_date}{tipo_doc}{documen}{tipFACB}{digit}{parse_cod_lug_emi}{num_comp_parsed}{imp}{porcent_alicuo}{imp_gr}"
                     t= dt.strip()
@@ -106,8 +112,6 @@ def read_file(file_name) :
             elif tipo_compr == 'N/C' and digit == 'A':
                  tipNCA = "03"
                  
-                 #print("soy tipNCA",tipNCA)
-                 
                  with open("DATOS.TXT", "at") as d:
                     dt = f"{parsed_date}{tipo_doc}{documen}{tipNCA}{digit}{parse_cod_lug_emi}{num_comp_parsed}{imp}{porcent_alicuo}{imp_gr}"
                     t= dt.strip()
@@ -115,8 +119,7 @@ def read_file(file_name) :
                  
             elif tipo_compr =='N/C' and digit == 'B':
                  tipNCB = "08"
-                 #print("soy tipNCB",tipNCB)
-                 
+                                 
                  with open("DATOS.TXT", "at") as d:
                     dt = f"{parsed_date}{tipo_doc}{documen}{tipNCB}{digit}{parse_cod_lug_emi}{num_comp_parsed}{imp}{porcent_alicuo}{imp_gr}"
                     t= dt.strip()
@@ -124,8 +127,7 @@ def read_file(file_name) :
               
             elif tipo_compr =='DEB' and digit == 'A':
                  tipDEBA = "02"
-                 #print("soy tipDEBA",tipDEBA)
-                 
+                                 
                  with open("DATOS.TXT", "at") as d:
                     dt = f"{parsed_date}{tipo_doc}{documen}{tipDEBA}{digit}{parse_cod_lug_emi}{num_comp_parsed}{imp}{porcent_alicuo}{imp_gr}"
                     t= dt.strip()
